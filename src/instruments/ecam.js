3ed3a0d8d03bf18801a79958d20c0c1d2b0e409a
luq-mne <luqman.yazrie@outlook.com>
2025-01-09 19:10:19 +0800

    some work on the ecam

--- src/instruments/ecam.js
+++ src/instruments/ecam.js
@@ -1,39 +1,20 @@
 import { InstrumentFrame } from "./frames";
 import { ButtonText } from "../components/buttons";
 
+let data = {
+    mode: "general",
+    page: "home",
+    title: "",
+    text: "",
+    buttons: {
+        1: "",
+        2: "",
+        3: "",
+        4: "",
+    }
+}
+
 export default function EcamControls(k, x) {
-    let mode = "general";
-    let page = "home";
-    let data = {
-        general: {
-            home: {
-                title: "GENERAL - HOME",
-                text: `
-Welcome to XFS 350. Please note this is still a work in progress!
-
-This display mode for the ECAM display does not exist in the real aircraft - this is designed to assist you with the simulator using guides and various controls.
-
-Text highlighted as blue performs an action when clicked.`,
-                buttons: {
-                    1: "",
-                    2: "",
-                    3: "",
-                    4: ""
-                }
-            },
-            menu: {
-                title: "GENERAL - MENU",
-                text: "",
-                buttons: {
-                    1: "",
-                    2: "",
-                    3: "",
-                    4: ""
-                }
-            }
-        }
-    };
-
     InstrumentFrame(k, "ECAM/GENERAL PURPOSE DISPLAY", x, k.height() - 5, 230, 280);
 
     const display = k.add([
@@ -68,17 +49,34 @@ Text highlighted as blue performs an action when clicked.`,
     // 
 
     // Fixed buttons
-    const homeButton = ButtonText(k, "Home", "right", x + 10, k.height() - 15, () => { console.log("home") });
-    const menuButton = ButtonText(k, "Menu", "right", x + 50, k.height() - 15, () => { console.log("menu") }); 
+    const homeButton = ButtonText(k, "Home", "right", x + 10, k.height() - 15, () => { data.page = "home" });
+    const menuButton = ButtonText(k, "Menu", "right", x + 50, k.height() - 15, () => { data.page = "menu" }); 
     const settingsButton = ButtonText(k, "Settings", "right", x + 90, k.height() - 15, () => { console.log("settings") });
     const aboutButton = ButtonText(k, "About", "right", x + 155, k.height() - 15, () => { console.log("about") });
 
     k.onUpdate(() => {
-        title.text = data[mode][page].title;
-        text.text = data[mode][page].text;
+        title.text = data.title;
+        text.text = data.text;
 
-        if (page == "home") {
-
+        if (data.mode == "general") {
+            GeneralMode(k);
         }
     });
+};
+
+function GeneralMode(k) {
+    switch (data.page) {
+        case "home":
+            data.title = "HOME",
+            data.text = `Welcome to XFS 350. Please note this is still a work in progress!
+
+This display mode for the ECAM display does not exist in the real aircraft - this is designed to assist you with the simulator using guides and various controls.
+
+Text highlighted as blue performs an action when clicked.`
+            break;
+        
+        case "menu":
+            data.title = "MENU",
+            data.text = "Menu stuff"
+    };
 };
