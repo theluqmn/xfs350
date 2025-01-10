f915e175f145287d1e2c52bd138fdca1c9b7c40d
luq-mne <luqman.yazrie@outlook.com>
2025-01-10 15:37:44 +0800

    throttle thingy!

--- src/instruments/controls.js
+++ src/instruments/controls.js
@@ -3,7 +3,7 @@ import { simData } from "../functions/simEngine";
 
 export default function InsControls(k, x) {
     brakeIndicator(k, x);
-    InstrumentFrame(k, "THROTTLE", x + 55, k.height() - 135, 100, 150);
+    throttleIndicator(k, x);
     InstrumentFrame(k, "FLAPS", x + 160, k.height() - 135, 50, 150);
     InstrumentFrame(k, "ENGINES", x, k.height() - 5, 210, 115);
 };
@@ -12,7 +12,7 @@ function brakeIndicator(k, x) {
     InstrumentFrame(k, "BRAKES", x, k.height() - 135, 50, 150);
 
     const guide = k.add([
-        k.rect(10,130),
+        k.rect(5,130),
         k.pos(x + 25, k.height() - 145),
         k.color(200,200,200),
         k.anchor("bot")
@@ -27,5 +27,42 @@ function brakeIndicator(k, x) {
 
     k.onUpdate(() => {
         indicator.pos.y = k.height() - 145 - (simData.inputs.brakes * 110 / 100);
-    })
+    });
+};
+
+function throttleIndicator(k, x) {
+    InstrumentFrame(k, "THROTTLE", x + 55, k.height() - 135, 100, 150);
+
+    const guideLeft = k.add([
+        k.rect(5,130),
+        k.pos(x + 82, k.height() - 145),
+        k.color(200,200,200),
+        k.anchor("bot")
+    ]);
+
+    const guideRight = k.add([
+        k.rect(5,130),
+        k.pos(x + 128, k.height() - 145),
+        k.color(200,200,200),
+        k.anchor("bot")
+    ]);
+
+    const indicatorLeft = k.add([
+        k.rect(40,20),
+        k.pos(x + 82, k.height() - 145),
+        k.color(10,10,10),
+        k.anchor("bot")
+    ]);
+
+    const indicatorRight = k.add([
+        k.rect(40,20),
+        k.pos(x + 128, k.height() - 145),
+        k.color(10,10,10),
+        k.anchor("bot")
+    ]);
+
+    k.onUpdate(() => {
+        indicatorLeft.pos.y = k.height() - 145 - (simData.inputs.throttle * 110 / 100);
+        indicatorRight.pos.y = k.height() - 145 - (simData.inputs.throttle * 110 / 100);
+    });
 }
