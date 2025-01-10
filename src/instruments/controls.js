804b04151ceb93d7da7ad7a663294dcdef7eaea8
luq-mne <luqman.yazrie@outlook.com>
2025-01-10 15:49:01 +0800

    flaps thingy

--- src/instruments/controls.js
+++ src/instruments/controls.js
@@ -4,7 +4,7 @@ import { simData } from "../functions/simEngine";
 export default function InsControls(k, x) {
     brakeIndicator(k, x);
     throttleIndicator(k, x);
-    InstrumentFrame(k, "FLAPS", x + 160, k.height() - 135, 50, 150);
+    flapsIndicator(k, x);
     InstrumentFrame(k, "ENGINES", x, k.height() - 5, 210, 115);
 };
 
@@ -12,9 +12,9 @@ function brakeIndicator(k, x) {
     InstrumentFrame(k, "BRAKES", x, k.height() - 135, 50, 150);
 
     const guide = k.add([
-        k.rect(5,130),
+        k.rect(5,80),
         k.pos(x + 25, k.height() - 145),
-        k.color(200,200,200),
+        k.color(20,20,20),
         k.anchor("bot")
     ]);
 
@@ -26,7 +26,7 @@ function brakeIndicator(k, x) {
     ]);
 
     k.onUpdate(() => {
-        indicator.pos.y = k.height() - 145 - (simData.inputs.brakes * 110 / 100);
+        indicator.pos.y = k.height() - 145 - (simData.inputs.brakes * 60 / 100);
     });
 };
 
@@ -36,33 +36,55 @@ function throttleIndicator(k, x) {
     const guideLeft = k.add([
         k.rect(5,130),
         k.pos(x + 82, k.height() - 145),
-        k.color(200,200,200),
+        k.color(20,20,20),
         k.anchor("bot")
     ]);
 
     const guideRight = k.add([
         k.rect(5,130),
         k.pos(x + 128, k.height() - 145),
-        k.color(200,200,200),
+        k.color(20,20,20),
         k.anchor("bot")
     ]);
 
     const indicatorLeft = k.add([
-        k.rect(40,20),
+        k.rect(40,25),
         k.pos(x + 82, k.height() - 145),
-        k.color(10,10,10),
+        k.color(50,50,50),
         k.anchor("bot")
     ]);
 
     const indicatorRight = k.add([
-        k.rect(40,20),
+        k.rect(40,25),
         k.pos(x + 128, k.height() - 145),
-        k.color(10,10,10),
+        k.color(50,50,50),
         k.anchor("bot")
     ]);
 
     k.onUpdate(() => {
         indicatorLeft.pos.y = k.height() - 145 - (simData.inputs.throttle * 110 / 100);
         indicatorRight.pos.y = k.height() - 145 - (simData.inputs.throttle * 110 / 100);
+    });
+};
+
+export function flapsIndicator(k, x) {
+    InstrumentFrame(k, "FLAPS", x + 160, k.height() - 135, 50, 150);
+
+    const guide = k.add([
+        k.rect(5,80),
+        k.pos(x + 185, k.height() - 145),
+        k.color(20,20,20),
+        k.anchor("bot")
+    ]);
+
+    const indicator = k.add([
+        k.rect(40,20),
+        k.pos(x + 185, k.height() - 145),
+        k.color(50,50,50),
+        k.anchor("bot")
+    ]);
+
+    k.onUpdate(() => {
+        indicator.pos.y = k.height() - 145 - (simData.inputs.flaps * 60 / 40);
     });
 }
